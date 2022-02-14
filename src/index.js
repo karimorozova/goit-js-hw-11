f971ac4681399ed7c237d213779d452a8993247a
karine-morozova <morozova_karine@ukr.net>
2022-02-14 13:57:53 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,3 +3,22 @@ import Notiflix from 'notiflix';
 
 // one by one
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// fetch('https://pixabay.com/api/?key=25701061-595d2fe4965b481dc05c5d7ff&q=iphone&image_type=photo').then(res=>res.json()).catch(console.log)
+
+const axios = require('axios');
+
+// axios.get('https://pixabay.com/api/?key=25701061-595d2fe4965b481dc05c5d7ff&q=iphones&image_type=photo')
+
+// async function getUser() {
+//     try {
+//       const response = await axios.get('https://pixabay.com/api/?key=25701061-595d2fe4965b481dc05c5d7ff&q=iphone&image_type=photo');
+//       console.log(response);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }
+
+//   getUser()
+
+fetch('https://pixabay.com/api/?key=25701061-595d2fe4965b481dc05c5d7ff&q=yellow+flowers&image_type=photo').then(res=>res.json())
